7bd8f9be83a707ef8f5c8bcb68a9eb97bc3ffb5f
Will Fox <william.macdonald.fox@gmail.com>
2024-10-20 21:33:37 -0400

    test: update facility tests with registrant instance variables

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+    @registrant_2 = Registrant.new('Penny', 16)
+    @registrant_3 = Registrant.new('Tucker', 15)
   end
 
   describe '#initialize' do
