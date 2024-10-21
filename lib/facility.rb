03393c32ab1c7a2ca787fa6e8cb43dcd1b8bb254
Will Fox <william.macdonald.fox@gmail.com>
2024-10-20 22:34:56 -0400

    feat: implement register vehicle method to track registrations and fees

--- lib/facility.rb
+++ lib/facility.rb
@@ -7,9 +7,30 @@ class Facility
     @address = details[:address]
     @phone = details[:phone]
     @services = []  
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    return nil unless @services.include?('Vehicle Registration')
+
+    vehicle.registration_date = Date.today
+
+    if vehicle.year <= Date.today.year - 25
+      vehicle.plate_type = :antique
+      @collected_fees += 25
+    elsif vehicle.engine == :ev
+      vehicle.plate_type = :ev
+      @collected_fees += 200
+    else
+      vehicle.plate_type = :regular
+      @collected_fees += 100
+    end
+
+    @registered_vehicles << vehicle
   end
 end
