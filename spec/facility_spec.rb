9e0daf2e1a7f1fbb79a277cd13bec0cb4b9c4833
Will Fox <william.macdonald.fox@gmail.com>
2024-10-20 20:54:20 -0400

    test: add vehicle registration tests with fee tracking

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Facility do
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
   end
-  
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
@@ -22,10 +22,43 @@ RSpec.describe Facility do
   describe '#add service' do
     it 'can add available services' do
       expect(@facility.services).to eq([])
-      @facility.add_service('New Drivers License')
-      @facility.add_service('Renew Drivers License')
-      @facility.add_service('Vehicle Registration')
-      expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+
+      @facility_1.add_service('New Drivers License')
+      @facility_1.add_service('Renew Drivers License')
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
+  end
+
+  describe '#register_vehicle' do
+    it 'can register a vehicle and collect fees' do
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
+
+      facility_1.register_vehicle(@cruz)
+      
+      expect(facility_1.registered_vehicles).to eq([cruz])
+      expect(@cruz.registration_date).to be_a(Date) 
+      expect(@cruz.plate_type).to eq(:regular)
+      expect(@facility_1.collected_fees).to eq(100)
+
+      facility_1.register_vehicle(@camaro)
+
+      expect(facility_1.registered_vehicles).to include(@camaro)
+      expect(@camaro.registration_date).to be_a(Date) 
+      expect(@camaro.plate_type).to eq(:antique)
+      expect(facility_1.collected_fees).to eq(125) 
+
+      facility_1.register_vehicle(@bolt)
+
+      expect(facility_1.registered_vehicles).to include(@bolt)
+      expect(@bolt.registration_date).to be_a(Date) 
+      expect(@bolt.plate_type).to eq(:ev)
+      expect(facility_1.collected_fees).to eq(325) 
+    end
+
+
+    
   end
 end
