e9cb68f545c57bed4aebd28174cd614d4e685155
Will Fox <william.macdonald.fox@gmail.com>
2024-10-21 21:31:17 -0400

    test: complete VehicleFactory class to generate vehicles from API data

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,1 +1,15 @@
+ require './lib/vehicle'
+ 
  class VehicleFactory   
+    def create_vehicles(data)
+        data.map do |entry|
+            Vehicle.new(
+                vin: entry[:vin_1_10], 
+                year: entry[:model_year],
+                make: entry[:make],
+                model: entry[:model],
+                engine: :ev
+            )
+        end
+    end
+ end
