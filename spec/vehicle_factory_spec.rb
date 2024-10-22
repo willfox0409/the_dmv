965ba58aa8d3dd087c7dd9844f471c2cfb1ef7b8
Will Fox <william.macdonald.fox@gmail.com>
2024-10-21 20:58:32 -0400

    test: add VehicleFactory spec to validate vehicle creation from wa ev database

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,2 +1,22 @@
 require 'spec_helper'
 
+RSpec.describe VehicleFactory do
+  before(:each) do
+    @factory = VehicleFactory.new
+    @service = DmvDataService.new
+    @wa_ev_data = @service.wa_ev_registrations
+  end
+
+  describe '#create_vehicles' do
+    it 'creates vehicle objects from the wa ev data' do
+      vehicles = @factory.create_vehicles(@wa_ev_data)
+
+      expect(vehicles).to be_an(array)
+      expect(vehicles.first).to be_a(vehicle)
+      expect(vehicles.first.engine).to eq(:ev)
+      expect(vehicles.first.make).to eq('TOYOTA')
+    end
+  end 
+end
+
+
