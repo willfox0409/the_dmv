7e95197cc70154420cc392037c793c2abac634f1
Will Fox <william.macdonald.fox@gmail.com>
2024-10-22 18:46:15 -0400

    feat: refactored 'create_facilities' to dynamically handle both co and ny data, ensured fallback values for missing fields

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,30 +3,67 @@ require './lib/facility'
 class FacilityFactory
   def create_facilities(data)
     data.map do |entry|
-      Facility.new(
-        name: entry[:dmv_office],
-        address: format_address(entry),
-        phone: entry[:phone],
-        services: entry[:services_p],
-        hours: format_hours(entry[:hours]),
-        address_id: entry[:address_id]
-      )
+
+    Facility.new(
+      name: entry[:dmv_office] || entry[:office_name],
+      address: format_address(entry),
+      phone: entry[:phone] || entry[:public_phone_number] || "Phone not available",
+      services: entry[:services_p] || "No services listed",
+      hours: format_hours(entry),
+      address_id: entry[:address_id] || entry[:unique_id] || "ID not available"
+    )
     end
   end
-
+    
   def format_address(entry)
-    "#{entry[:address_li]}, #{entry[:address__1]}, #{entry[:city]}, #{entry[:state]} #{entry[:zip]}"
-  end
-
-  def format_services(service_string)
-    service_string || ""
-  end
-
-  def format_hours(hours_string)
-    if hours_string
-      hours_string
-    else
-      "Hours not available"
+      if entry[:address_li]
+        "#{entry[:address_li]}, #{entry[:address__1]}, #{entry[:city]}, #{entry[:state]} #{entry[:zip]}"
+        else
+          "#{entry[:street_address_line_1]}, #{entry[:city]}, #{entry[:state]} #{entry[:zip_code]}"
+        end
+      end
+    
+      def format_hours(entry)
+        if entry[:monday_beginning_hours]
+          "Mon: #{entry[:monday_beginning_hours]} - #{entry[:monday_ending_hours]}"
+        else
+          entry[:hours] || "Hours not available"
+        end
     end
-  end
-end
+end       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #       Facility.new(
+#         name: entry[:dmv_office] || entry[:office_name],
+#         address: format_address(entry),
+#         phone: entry[:phone] || entry[:public_phone_number] || "Phone not available",
+#         services: entry[:services_p] || "No services listed",
+#         hours: format_hours(entry),
+#         address_id: entry[:address_id] || entry[:unique_id] || "ID not available"
+#       )
+#     end
+#   end
+
+#   def format_address(entry)
+#     if entry[:address_li]
+#       "#{entry[:address_li]}, #{entry[:address__1]}, #{entry[:city]}, #{entry[:state]} #{entry[:zip]}"
+#     else
+#       "#{entry[:street_address_line_1]}, #{entry[:city]}, #{entry[:state]} #{entry[:zip_code]}"
+#     end
+#   end
+
+#   def format_hours(entry)
+#     if entry[:monday_beginning_hours]
+#       "Mon: #{entry[:monday_beginning_hours]} - #{entry[:monday_ending_hours]}"
+#     else
+#       entry[:hours] || "Hours not available"
+#     end
+#   end
+# end
