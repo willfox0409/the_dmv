26e57a1c81b111ed97c5c865c80a97f2cb87ff68
Will Fox <william.macdonald.fox@gmail.com>
2024-12-12 20:52:35 -0500

    fix: correct typo in add_service method, removed extra s character

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,14 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(info)
+    @name = info[:name]
+    @address = info[:address]
+    @phone = info[:phone]
     @services = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
