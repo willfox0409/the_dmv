b433803929a9ac14e7cb682dd7a6981bbe125d92
Will Fox <william.macdonald.fox@gmail.com>
2024-12-12 21:52:50 -0500

    test: add tests for Registrant attributes

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 RSpec.describe Registrant do 
+    before(:each) do 
+        @registrant_1 = Registrant.new('Bruce', 18, true )
+        @registrant_2 = Registrant.new('Penny', 15 )
+    end
     describe '#initialize' do 
         it 'exists' do 
-            registrant_1 = Registrant.new('Bruce', 18, true )
-            expect(registrant_1).to be_a(Registrant)
+            expect(@registrant_1).to be_a(Registrant)
+        end
+
+        it 'has a name' do 
+            expect(@registrant_1.name).to eq("Bruce") 
+        end
+
+        it 'has an age' do 
+            expect(@registrant_1.age).to eq(18)
+        end
+
+        it 'has a permit' do 
+            expect(@registrant_1.permit?).to eq(true)
         end
     end
 end
