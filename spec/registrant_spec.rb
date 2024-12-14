e2e8df4585b39b2f2225e44efaf0e7cee4e72c1f
Will Fox <william.macdonald.fox@gmail.com>
2024-12-13 19:19:32 -0500

    test: update permit? method test to be more comprehensive, testing for registrant_2  permit to default to false

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Registrant do
         @registrant_1 = Registrant.new('Bruce', 18, true )
         @registrant_2 = Registrant.new('Penny', 15 )
     end
+
     describe '#initialize' do 
         it 'exists' do 
             expect(@registrant_1).to be_a(Registrant)
@@ -20,6 +21,16 @@ RSpec.describe Registrant do
 
         it 'has a permit' do 
             expect(@registrant_1.permit?).to eq(true)
+        end
+
+        it 'defaults permit to false when not provided' do
+            expect(@registrant_2.name).to eq("Penny")
+            expect(@registrant_2.age).to eq(15)
+            expect(@registrant_2.permit?).to eq(false)
+        end
+
+        it 'has license_data' do 
+            expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
         end
     end
 end
