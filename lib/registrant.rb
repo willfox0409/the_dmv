1db276543b5d55619701fd37f3f887f8f5e23528
Will Fox <william.macdonald.fox@gmail.com>
2024-12-13 19:17:39 -0500

    feat: update license_data hash to match interaction pattern

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -5,7 +5,7 @@ class Registrant
         @name = name
         @age = age 
         @permit = permit 
-        @license_data = {}
+        @license_data = {:written => false, :license => false, :renewed => false}
     end
 
     def permit?
