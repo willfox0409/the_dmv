0432f8f3b443542e1fcd2fb6a0912b64bda112fe
Will Fox <william.macdonald.fox@gmail.com>
2024-12-15 13:16:21 -0500

    feat: implement renew_drivers_license method in Facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -52,4 +52,12 @@ class Facility
       false
     end
   end
+
+  def renew_drivers_license(registrant)
+    if @services.include?('Renew License') && registrant.license_data[:license] == true
+      registrant.license_data[:renewed] = true
+    else
+      false
+    end
+  end
 end
