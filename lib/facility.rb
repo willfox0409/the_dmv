2b4fa0ed5b37f9a9e27a4e1ec1f1e1ccdcc05032
Will Fox <william.macdonald.fox@gmail.com>
2024-12-14 20:51:26 -0500

    fix: implement additional logic to register_vehicle method to only execute if the facility includes vehicle registration as a service

--- lib/facility.rb
+++ lib/facility.rb
@@ -15,22 +15,25 @@ class Facility
   end
 
   def register_vehicle(vehicle) # refactor to be more concise, combine these conditional blocks?
-    vehicle.registration_date = Date.today
-    @registered_vehicles << vehicle 
-   if vehicle.antique?
-      vehicle.plate_type = :antique
-   elsif vehicle.electric_vehicle?
-      vehicle.plate_type = :ev
-   else
-      vehicle.plate_type = :regular
-   end
+    if @services.include?('Vehicle Registration')
 
-   if vehicle.plate_type == :ev
-      @collected_fees += 200
-   elsif vehicle.plate_type == :antique
-      @collected_fees += 25
-   else
-      @collected_fees += 100
-   end
+      vehicle.registration_date = Date.today
+      @registered_vehicles << vehicle 
+      if vehicle.antique?
+        vehicle.plate_type = :antique
+      elsif vehicle.electric_vehicle?
+        vehicle.plate_type = :ev
+      else
+      vehicle.plate_type = :regular
+      end
+
+      if vehicle.plate_type == :ev
+        @collected_fees += 200
+      elsif vehicle.plate_type == :antique
+        @collected_fees += 25
+      else
+        @collected_fees += 100
+      end
+    end
   end
 end
