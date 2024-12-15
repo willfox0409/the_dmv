60d360da1d441c5db224f849ce9b7e0ff510fbef
Will Fox <william.macdonald.fox@gmail.com>
2024-12-15 15:04:23 -0500

    test: add edge case tests for license renewal

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -167,5 +167,27 @@ RSpec.describe Facility do
       expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(true)
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
     end
+
+    it 'does not allow renewal if registrant does not have a license' do
+      @facility_1.add_service('Renew License')
+      
+      expect(@facility_1.renew_drivers_license(@registrant_3)).to eq(false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    it 'does not renew a license if the service is not offered' do
+      expect(@facility_1.services).to eq([])
+    
+      @facility_1.add_service('Written Test')
+      @facility_1.add_service('Road Test')
+    
+      expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
+      expect(@facility_1.administer_road_test(@registrant_1)).to eq(true)
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    
+      expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(false)
+    
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
   end
 end
