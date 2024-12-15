15f9dc98a348befc0dc83b533b7e54e9f45bdb00
Will Fox <william.macdonald.fox@gmail.com>
2024-12-14 20:02:03 -0500

    test: add more comprehensive tests for register_vehicle method, updated test names to be more precise

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Facility do
   end
 
   describe '#initialize' do
-    it 'can initialize' do
+    it 'initializes with attributes' do
       expect(@facility_1).to be_an_instance_of(Facility)
       expect(@facility_1.name).to eq('DMV Tremont Branch')
       expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
@@ -22,28 +22,54 @@ RSpec.describe Facility do
   end
 
   describe '#add service' do
-    it 'can add available services' do
+    it 'adds available services' do
       expect(@facility_1.services).to eq([])
+
       @facility_1.add_service('New Drivers License')
       @facility_1.add_service('Renew Drivers License')
       @facility_1.add_service('Vehicle Registration')
+
       expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
 
   describe '#register_vehicle' do 
-    it 'can register a vehicle at a specific dmv facility' do 
+    it 'registers a single vehicle to the facility' do 
       expect(@facility_1.registered_vehicles).to eq([])
 
       @facility_1.register_vehicle(@cruz)
+
       expect(@facility_1.registered_vehicles).to eq([@cruz])
     end
 
-    it 'can update registration_date attribute once registered' do 
+    it 'updates registration_date for the registered vehicle' do 
       expect(@cruz.registration_date).to eq(nil)
 
       @facility_1.register_vehicle(@cruz)
+
       expect(@cruz.registration_date).to eq(Date.today)
+    end
+
+    it 'registers multiple vehicles at the facility' do 
+      expect(@facility_1.registered_vehicles).to eq([])
+
+
+      @facility_1.register_vehicle(@cruz)
+      @facility_1.register_vehicle(@camaro)
+
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro])
+    end
+
+    it 'assigns the correct plate_type based on vehicle age and engine type' do 
+      @facility_1.register_vehicle(@cruz)
+      @facility_1.register_vehicle(@camaro)
+      @facility_1.register_vehicle(@bolt)
+
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
+
+      expect(@cruz.plate_type).to eq(:regular)
+      expect(@camaro.plate_type).to eq(:antique)
+      expect(@bolt.plate_type).to eq(:ev)
     end
   end
 end
