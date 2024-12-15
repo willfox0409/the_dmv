6d3d346878de474330d3e166e6be84aba55ffbfe
Will Fox <william.macdonald.fox@gmail.com>
2024-12-14 20:03:26 -0500

    feat: add plate_type to attr_accessor instead of attr_reader

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,8 @@ class Vehicle
               :make,
               :model,
               :engine
-  attr_accessor :registration_date
+  attr_accessor :registration_date, 
+                :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -15,6 +16,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = nil
+    @plate_type = :regular 
   end
 
   def antique?
