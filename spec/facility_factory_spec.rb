7795dc80a7bcf507a941db711d433ed4735cd138
Will Fox <william.macdonald.fox@gmail.com>
2024-12-17 16:39:13 -0500

    test: implement tests for ny data in create_facilities test, using mock data that matches the real data, all tests passing

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do 
     before(:each) do 
         @facility_factory = FacilityFactory.new
-        @source = :co
+        @co_source = :co
+        @ny_source = :ny
+
         @co_mock_data = [
             {
                 dmv_office: "DMV Southwest Branch",
@@ -16,11 +18,24 @@ RSpec.describe FacilityFactory do
                 phone: "(720) 865-4600"
             }
         ]
+
+        @ny_mock_data = [
+            {
+                office_name: "LAKE PLACID",
+                street_address_line_1: "2693 MAIN STREET",
+                public_phone_number: "5188283350"
+            },
+            {
+                office_name: "HUDSON",
+                street_address_line_1: "560 WARREN STREET",
+                public_phone_number: "5188283350"
+            }
+        ]
     end
 
     describe 'create_facilities' do 
-      it 'creates an array of facilities from the raw colorado data' do 
-            facilities = @facility_factory.create_facilities(@co_mock_data, @source)
+        it 'creates an array of facilities from Colorado data' do 
+            facilities = @facility_factory.create_facilities(@co_mock_data, @co_source)
 
             expect(facilities).to be_an(Array)
             expect(facilities.size).to eq(2)
@@ -36,6 +51,25 @@ RSpec.describe FacilityFactory do
             expect(second_facility.name).to eq("DMV Southeast Branch")
             expect(second_facility.address).to eq("2243 S Monaco Street Pkwy")
             expect(second_facility.phone).to eq("(720) 865-4600")
+        end
+
+        it 'creates facilities from New York data' do 
+            facilities = @facility_factory.create_facilities(@ny_mock_data, @ny_source)
+
+            expect(facilities).to be_an(Array)
+            expect(facilities.size).to eq(2)
+
+            first_facility = facilities.first
+            expect(first_facility).to be_a(Facility)
+            expect(first_facility.name).to eq("LAKE PLACID")
+            expect(first_facility.address).to eq("2693 MAIN STREET")
+            expect(first_facility.phone).to eq("5188283350")
+
+            second_facility = facilities.last
+            expect(second_facility).to be_a(Facility)
+            expect(second_facility.name).to eq("HUDSON")
+            expect(second_facility.address).to eq("560 WARREN STREET")
+            expect(second_facility.phone).to eq("5188283350")
         end
     end
 end
